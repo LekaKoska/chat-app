81a046429d9c467398ca582a5bb97ed1d3218cde
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-15 12:28:08 +0200

    * Adding Intervention Image package for picture compression * Editing view * Creating method avatar in ProfileController for adding profile picture

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,28 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\NewAvatarRequest;
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Traits\UploadImage;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 
 class ProfileController extends Controller
 {
-    /**
-     * Display the user's profile form.
-     */
+    use UploadImage;
     public function edit(Request $request): View
     {
         return view('profile.edit', [
             'user' => $request->user(),
         ]);
     }
-
-    /**
-     * Update the user's profile information.
-     */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
         $request->user()->fill($request->validated());
@@ -36,10 +33,6 @@ class ProfileController extends Controller
 
         return Redirect::route('profile.edit')->with('status', 'profile-updated');
     }
-
-    /**
-     * Delete the user's account.
-     */
     public function destroy(Request $request): RedirectResponse
     {
         $request->validateWithBag('userDeletion', [
@@ -56,5 +49,19 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    public function avatar(NewAvatarRequest $request)
+    {
+        $avatar = Auth::user()->avatar;
+        if($avatar !== null)
+        {
+            File::delete(paths: "storage/images/avatars/$avatar");
+        }
+        $name = $this->avatarUpload(file: $request->file('profile_image'), path: '/images/avatars/');
+
+        Auth::user()->update(['avatar' => $name]);
+
+        return redirect()->back();
     }
 }
