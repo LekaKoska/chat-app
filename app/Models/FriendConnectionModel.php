90e12aa0399728967a8d3a599dd043743b6123b0
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-15 09:13:21 +0200

    * casting enum in model

--- app/Models/FriendConnectionModel.php
+++ app/Models/FriendConnectionModel.php
@@ -2,15 +2,17 @@
 
 namespace App\Models;
 
+use App\Enums\FriendStatus;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class FriendConnectionModel extends Model
 {
     const TABLE = 'friends_connection';
+    protected $casts = [ 'status' => FriendStatus::class];
 
     protected $table = self::TABLE;
-    protected $fillable = ['sender_id', 'receiver_id', 'status'];
+    protected $fillable = ['sender_id', 'receiver_id'];
 
     public function sender(): BelongsTo
     {
