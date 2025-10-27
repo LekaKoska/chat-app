5599a670920b035a200a7519ded4d3f36d1909d9
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-10-27 14:32:48 +0100

    * Creating Friend request and request accepted notifications * Creating Friend request observer * Register new observer

---
+++ app/Notifications/FriendRequestAccepted.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+
+class FriendRequestAccepted extends Notification
+{
+    use Queueable;
+    protected User $accepter;
+    public function __construct(User $accepter)
+    {
+        $this->accepter = $accepter;
+    }
+
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+    public function toDatabase(object $notifiable): array
+    {
+        return [
+            'type' => 'friend_request_accepted',
+            'notification' => "{$this->accepter->name} accepted your friend request",
+            'url' => route('friends.request.index'),
+            'sender' => [
+                'id' => $this->accepter->id,
+                'name' => $this->accepter->name,
+                'avatar' => $this->accepter->avatar ?? null,
+            ]
+        ];
+    }
+}
