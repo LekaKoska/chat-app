8bb4eddfb1236f5dc072e77f66b4791be9730a94
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-11-04 13:14:08 +0100

    * Creating new table for messages * Creating Controller, Request and Model for messages * Creating Event and Broadcast

---
+++ app/Models/Message.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Message extends Model
+{
+    const TABLE = 'messages';
+    protected  $table = self::TABLE;
+    protected $fillable = [
+        'sender_id',
+        'receiver_id',
+        'message',
+        'read'
+    ];
+
+    public function sender(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+    public function receiver(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
+}
