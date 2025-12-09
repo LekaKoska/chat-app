618f3e348b9ed4e868437f8280d20a22ca6b5372
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-09 14:15:07 +0100

    * Editing Factories for Docker

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -2,22 +2,15 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
-
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Comment>
- */
 class CommentFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            //
+            'comment' => $this->faker->realText(50)
         ];
     }
 }
