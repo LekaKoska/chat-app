25b2abc97ecb2b0787a5e415d372c500d9d3a583
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2025-12-09 14:16:29 +0100

    * Calling all Factories in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\ReplyComment;
 use App\Models\User;
+use App\Models\Vote;
+use Database\Factories\CommentFactory;
 use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -14,11 +19,35 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users = User::factory()->count(5)->create();
+        $users->each(function ($user) {
+            Post::factory()->count(2)->create([
+                'user_id' => $user->id
+            ]);
+        });
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        Post::all()->each(function ($post) use ($users) {
+            Comment::factory()->count(1)->create([
+                'post_id' => $post->id,
+                'user_id' => $users->random()->id,
+            ]);
+        });
+
+        Comment::all()->each(function ($comment) use ($users) {
+            ReplyComment::factory()->count(1)->create([
+                'comment_id' => $comment->id,
+                'user_id' => $users->random()->id,
+            ]);
+        });
+        Post::all()->each(function ($post) use ($users) {
+            $users->random(rand(1, 4))->each(function ($user) use ($post) {
+                Vote::factory()->create([
+                    'post_id' => $post->id,
+                    'user_id' => $user->id,
+                    'vote' => fake()->randomElement([-1, 1]),
+                ]);
+            });
+        });
+
     }
 }
