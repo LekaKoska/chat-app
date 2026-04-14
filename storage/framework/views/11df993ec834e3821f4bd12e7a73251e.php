a46789ea450813ea83bd87834a8f3aeb3f766c7a
LekaKoska <159024082+LekaKoska@users.noreply.github.com>
2026-04-14 12:23:50 +0200

    * Implements register link into login form

--- storage/framework/views/11df993ec834e3821f4bd12e7a73251e.php
+++ storage/framework/views/11df993ec834e3821f4bd12e7a73251e.php
@@ -174,7 +174,16 @@
             </label>
         </div>
 
+
+
         <div class="flex items-center justify-end mt-4">
+
+            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800 mr-6"
+               href="<?php echo e(route('register')); ?>">
+                <?php echo e(__('Register right now')); ?>
+
+            </a>
+
             <?php if(Route::has('password.request')): ?>
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
                    href="<?php echo e(route('password.request')); ?>">
